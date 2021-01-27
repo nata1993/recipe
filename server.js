e7ce6a7b9e80922fff5818888782fd3a20f30e89
kebo <bogdan.parubok@hotmail.com>
2021-01-27 20:33:30 +0200

    m

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const ejs = require('ejs');
+const ax = require('axios');
+const port = 3000;
+
+// init app
+const app = express();
+
+app.set('view engine', ejs);
+app.use(express.static('public'));  // for css
+
+app.get('/', (req, res) => {
+    const randomRecipe =  "https://api.spoonacular.com/recipes/random?apiKey=75f02c3e61ba49ab94b9d4cc2e0cc2d5";
+    let title, image, summary, instructions;
+
+    ax.get(randomRecipe)
+    .then(function(response){
+        title = response.data.recipes[0].title;
+        image = response.data.recipes[0].image;
+        summary = response.data.recipes[0].summary;
+        instructions = response.data.recipes[0].instructions;
+
+        let _data = {
+            title,
+            image,
+            summary,
+            instructions
+        };
+
+        res.render('index.ejs', {
+            data: _data
+        });
+    })
+    .catch((error) =>{
+        console.log("Some error with request: " + error)
+    });
+});
+
+// app will run on this port
+app.listen(port, () => {
+    console.log(`app running on port ${port}\n`);
+});
+
+/*The name of the recipe
+Main ingredients (including amount and metric units)
+Summary
+Instructions
+Image*/
